c2ab6f5959f39cd703a85f5b619010331b0b36a0
Luis Ricardo Alves Santos <luisricardo2825@gmail.com>
2024-06-24 10:28:48 -0300

    Alteração

--- src/main/java/com/sankhya/ce/Main.java
+++ src/main/java/com/sankhya/ce/Main.java
@@ -1,15 +1,19 @@
 package com.sankhya.ce;
 
 import com.sankhya.ce.http.Http;
+import com.sankhya.ce.json.JsonHelper;
 import okhttp3.Headers;
 import org.apache.commons.lang3.tuple.Triple;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Triple<String, Headers, List<String>> response = Http.client.get("https://jsonplaceholder.typicode.com/todos/1");
-        System.out.println(response.getLeft());
+        Triple<String, Headers, List<String>> response = Http.client.get("https://jsonplaceholder.typicode.com/users");
+        JsonHelper json = new JsonHelper(response.getLeft());
+        List<Double> list = ((List<?>) json.get()).stream().map(it -> (Double) new JsonHelper(it).get("id")).collect(Collectors.toList());
+        list.forEach(System.out::println);
     }
 }
